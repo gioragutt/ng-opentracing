bca443a1610dc921c57b3ad9941b1645d050c907
Giora Guttsait <giora111@gmail.com>
2020-07-31 23:29:23 +0300

    Clean up unit tests and add some more

--- projects/tracing/src/lib/tracing-interceptor.spec.ts
+++ projects/tracing/src/lib/tracing-interceptor.spec.ts
@@ -1,11 +1,13 @@
-import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { initGlobalTracer, SpanOptions, Tags } from 'opentracing';
-import { MockTracer, MockSpan } from 'opentracing/lib/mock_tracer';
-import { TracingModule } from './tracing.module';
-import { createTracingOptions } from './request-options';
+import { MockSpan, MockTracer } from 'opentracing/lib/mock_tracer';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { DefaultTracingOptions } from './default-options';
+import { createTracingOptions } from './request-options';
+import { TracingModule } from './tracing.module';
 
 // @ts-ignore
 const originalStartSpan = MockTracer.prototype._startSpan;
@@ -37,47 +39,62 @@ describe('TracingInterceptorService', () => {
   });
 
   it('should trace on an http request', () => {
-    http.get('/endpoint').subscribe();
+    http.get('/endpoint1').subscribe();
 
-    httpMock.expectOne('/endpoint')
-      .event(new HttpResponse<any>({ status: 200 }));
+    expectSuccessfulRequest(httpMock, '/endpoint1');
 
     const [span] = tracer.report().spans;
 
-    expect(span.operationName()).toBe('GET /endpoint');
+    expect(span.operationName()).toBe('GET /endpoint1');
     expect(span.tags()).toEqual({
       [Tags.HTTP_METHOD]: 'GET',
       [Tags.HTTP_STATUS_CODE]: 200,
-      [Tags.HTTP_URL]: '/endpoint',
+      [Tags.HTTP_URL]: '/endpoint1',
       [Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_CLIENT,
     });
   });
 
   it('should log events of the http request', () => {
-    http.get('/endpoint').subscribe();
+    http.get('/endpoint2').subscribe();
 
-    const request = httpMock.expectOne('/endpoint');
+    const request = httpMock.expectOne('/endpoint2');
     request.event({ type: HttpEventType.DownloadProgress, loaded: 5, total: 5 });
     request.event(new HttpResponse<any>({ status: 200 }));
 
     const [span] = tracer.report().spans;
-    console.log(span);
     expect(logsOf(span).length).toBe(3);
   });
 
   it('should skip trace of specified in params', () => {
-    http.get('/endpoint', { params: createTracingOptions({ skipTrace: true }) }).subscribe();
+    http.get('/endpoint3', { params: createTracingOptions({ skipTrace: true }) }).subscribe();
 
-    httpMock.expectOne('/endpoint')
-      .event(new HttpResponse<any>({ status: 200 }));
+    expectSuccessfulRequest(httpMock, '/endpoint3');
 
     expect(tracer.report().spans.length).toBe(0);
   });
 
-  it('should finish the span on the end of the http request', () => {
-    http.get('/endpoint').subscribe();
+  it('should set a custom span name when using spanName', () => {
+    http.get('/endpoint10', { params: createTracingOptions({ spanName: 'Span Name' }) }).subscribe();
 
-    const request = httpMock.expectOne('/endpoint');
+    expectSuccessfulRequest(httpMock, '/endpoint10');
+
+    expect(tracer.report().spans[0].operationName()).toBe('Span Name');
+  });
+
+  it('should use parent span if provided in parentSpan', () => {
+    const parentSpan = tracer.startSpan('parent-span');
+    http.get('/endpoint11', { params: createTracingOptions({ parentSpan }) }).subscribe();
+
+    expectSuccessfulRequest(httpMock, '/endpoint11');
+
+    const traceId = tracer.report().spans[0].context().toTraceId();
+    expect(traceId).toBe(parentSpan.context().toTraceId());
+  });
+
+  it('should finish the span on the end of the http request', () => {
+    http.get('/endpoint4').subscribe();
+
+    const request = httpMock.expectOne('/endpoint4');
     request.event(new HttpResponse<any>({ status: 200 }));
     request.flush(null);
 
@@ -85,9 +102,9 @@ describe('TracingInterceptorService', () => {
   });
 
   it('should set correct error tags and log on error', () => {
-    http.get('/endpoint').subscribe();
+    http.get('/endpoint5').pipe(catchError(() => of(true))).subscribe();
 
-    httpMock.expectOne('/endpoint').error(new ErrorEvent('kaki'));
+    httpMock.expectOne('/endpoint5').error(new ErrorEvent('kaki'));
     const [span] = tracer.report().spans;
 
     expect(span.tags()[Tags.ERROR]).toBeTrue();
@@ -96,65 +113,46 @@ describe('TracingInterceptorService', () => {
 
 describe('DefaultTracingOptions', () => {
   it('should not trace if enabled=true', () => {
-    const { http, httpMock, tracer } = reconfigureTracingModule({ enabled: false });
+    const { http, httpMock, tracer } = configureTracingModule({ enabled: false });
 
-    http.get('/endpoint').subscribe();
+    http.get('/endpoint6').subscribe();
 
-    httpMock.expectOne('/endpoint')
-      .event(new HttpResponse<any>({ status: 200 }));
+    expectSuccessfulRequest(httpMock, '/endpoint6');
 
     expect(tracer.report().spans.length).toBe(0);
   });
 
   it('should not log when logEvents=true', () => {
-    const { http, httpMock, tracer } = reconfigureTracingModule({ logEvents: false });
-    http.get('/endpoint').subscribe();
+    const { http, httpMock, tracer } = configureTracingModule({ logEvents: false });
+    http.get('/endpoint7').subscribe();
 
-    httpMock.expectOne('/endpoint')
-      .event(new HttpResponse<any>({ status: 200 }));
+    expectSuccessfulRequest(httpMock, '/endpoint7');
 
     const [span] = tracer.report().spans;
     expect((span as any)._logs as any[]).toEqual([]);
   });
 
   it('should set a custom span name when using spanName', () => {
-    const { http, httpMock, tracer } = reconfigureTracingModule({
+    const { http, httpMock, tracer } = configureTracingModule({
       spanName: () => 'Span Name',
     });
 
-    http.get('/endpoint').subscribe();
+    http.get('/endpoint8').subscribe();
 
-    httpMock.expectOne('/endpoint')
-      .event(new HttpResponse<any>({ status: 200 }));
+    expectSuccessfulRequest(httpMock, '/endpoint8');
 
     const [span] = tracer.report().spans;
     expect(span.operationName()).toBe('Span Name');
   });
 
   it('should not log events not included in logEvents', () => {
-    const { http, httpMock, tracer } = reconfigureTracingModule({
+    const { http, httpMock, tracer } = configureTracingModule({
       logEvents: [HttpEventType.UploadProgress, HttpEventType.Response],
     });
 
-    http.get('/endpoint').subscribe();
+    http.get('/endpoint9').subscribe();
 
-    const request = httpMock.expectOne('/endpoint');
-    request.event({ type: HttpEventType.UploadProgress, loaded: 5, total: 5 });
-    request.event({ type: HttpEventType.DownloadProgress, loaded: 5, total: 5 });
-    request.event(new HttpResponse<any>({ status: 200 }));
-
-    const [span] = tracer.report().spans;
-    expect(logsOf(span).length).toEqual(2);
-  });
-
-  it('should not log events not included in logEvents', () => {
-    const { http, httpMock, tracer } = reconfigureTracingModule({
-      logEvents: [HttpEventType.UploadProgress, HttpEventType.Response],
-    });
-
-    http.get('/endpoint').subscribe();
-
-    const request = httpMock.expectOne('/endpoint');
+    const request = httpMock.expectOne('/endpoint9');
     request.event({ type: HttpEventType.UploadProgress, loaded: 5, total: 5 });
     request.event({ type: HttpEventType.DownloadProgress, loaded: 5, total: 5 });
     request.event(new HttpResponse<any>({ status: 200 }));
@@ -164,21 +162,25 @@ describe('DefaultTracingOptions', () => {
   });
 
   it('should skip tracing if skipTrace returns true', () => {
-    const { http, httpMock, tracer } = reconfigureTracingModule({
+    const { http, httpMock, tracer } = configureTracingModule({
       skipTrace: req => req.url.includes('no-trace'),
     });
 
     http.get('/trace').subscribe();
     http.get('/no-trace').subscribe();
 
-    httpMock.expectOne('/trace');
-    httpMock.expectOne('/no-trace');
+    expectSuccessfulRequest(httpMock, '/trace');
+    expectSuccessfulRequest(httpMock, '/no-trace');
 
     const { spans } = tracer.report();
     expect(spans.length).toBe(1);
-    expect(spans[0].operationName()).toContain('trace');
+    expect(spans[0].operationName()).not.toContain('no-trace');
   });
 });
+
+function expectSuccessfulRequest(httpMock: HttpTestingController, url: string): void {
+  httpMock.expectOne(url).event(new HttpResponse<any>({ status: 200 }));
+}
 
 function logsOf(span: MockSpan): Array<{
   fields: { [key: string]: any };
@@ -188,7 +190,7 @@ function logsOf(span: MockSpan): Array<{
 }
 
 // tslint:disable-next-line: typedef
-function reconfigureTracingModule(opts?: DefaultTracingOptions) {
+function configureTracingModule(opts?: DefaultTracingOptions) {
   const tracer = new MockTracer();
   initGlobalTracer(tracer);
 
